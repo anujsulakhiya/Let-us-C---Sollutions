a7c11bcc6bf57a7e09ae134c30958c860b66b91d
Palkesh Sharma <58721387+Palkesh-ai@users.noreply.github.com>
2020-05-11 17:43:10 +0530

    Update Chapter 1- Geting Started [F].c

--- Chapter 1- Geting Started [F].c
+++ Chapter 1- Geting Started [F].c
@@ -6,12 +6,16 @@ int Sub1,Sub2,Sub3,Sub4,Sub5,AM,PER;
 
 printf("Enter marks obtained in Subjects");
 
-scanf("%d",&Sub1,&Sub2,&Sub3,&Sub4,&Sub5);
+scanf("%d%d%d%d%d",&Sub1,&Sub2,&Sub3,&Sub4,&Sub5);
 
 AM=(Sub1+Sub2+Sub3+Sub4+Sub5)/5;
 
-PER=(Sub1+Sub2+Sub3+Sub4+Sub5)/500*100;
+  printf("%d Agrigate marks obtained in subjects is",AM);
 
+  PER=(Sub1+Sub2+Sub3+Sub4+Sub5)/500*100;
+  
+printf("%d Total percentage obtained by student",PER);
+  printf("")
 return 0;
 
 }
