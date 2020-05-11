e885114a5b33e015f74007583a01239fc2abb587
Palkesh Sharma <58721387+Palkesh-ai@users.noreply.github.com>
2020-05-11 17:30:04 +0530

    Create Create Chapter 1- Geting Started [F].c

---
+++ Create Chapter 1- Geting Started [F].c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+int Sub1,Sub2,Sub3,Sub4,Sub5,AM,PER;
+
+printf("Enter marks obtained in Subjects");
+
+scanf("%d",&Sub1,&Sub2,&Sub3,&Sub4,&Sub);
+
+AM=(Sub1+Sub2+Sub3+Sub4+Sub5)/5;
+
+PER=(Sub1+Sub2+Sub3+Sub4+Sub5)/500*100;
+
+return 0;
+
+}
